66f75b41db4df6754506e493079ba5f75bab1d26
鳥越幸樹 <torigoekouki@torigoekoukinoMacBook-Air.local>
2023-05-21 14:38:59 +0900

    feat: TodoListPageの追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_riverpod/features/todo/presentation/todo_list_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,35 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TodoList'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      home: const TodoListPage(),
     );
   }
 }
