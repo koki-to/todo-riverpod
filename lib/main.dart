580e28d12a1ea1e1c7a23e31f4de60c20b9bd468
鳥越幸樹 <torigoekouki@torigoekoukinoMacBook-Air.local>
2023-05-21 23:12:54 +0900

    feat: SharedPreferencesのコードの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_riverpod/app.dart';
+import 'package:todo_riverpod/features/todo/application/todo_repository_impl.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // 画面の向きを固定する。
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
