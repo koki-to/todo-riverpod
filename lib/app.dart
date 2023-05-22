86e2119d1d8e1c004409aaac94684581dd9b445a
鳥越幸樹 <torigoekouki@torigoekoukinoMacBook-Air.local>
2023-05-22 22:27:52 +0900

    feat: themeMode切り替え機能の実装

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_riverpod/configs/theme_selector_provider.dart';
 import 'package:todo_riverpod/features/todo/presentation/todo_list_page.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.amber,
       ),
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Colors.red,
+      ),
+      themeMode: ref.watch(themeSelectorProvider),
       home: const TodoListPage(),
     );
   }
